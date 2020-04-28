c70c7b50387852d4013ac6a7e96bbaf5dda38bef
jieshuh2 <jieshuh2@illinois.edu>
2020-04-28 15:07:23 -0500

    New

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public int Teamoji;
     public int Sakanaya;
     public RadioGroup checkbox;
+    public Button start;
     public View map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         map = findViewById(R.id.mapView2);
         map.setVisibility(View.GONE);
+        RadioButton starving = findViewById(R.id.radioButton6);
+        RadioButton diet = findViewById(R.id.radioButton9);
+        RadioButton spicy = findViewById(R.id.radioButton7);
+        RadioButton quick = findViewById(R.id.radioButton8);
+        RadioButton vegetarian = findViewById(R.id.radioButton10);
+        RadioButton asian = findViewById(R.id.radioButton11);
+        RadioButton american = findViewById(R.id.radioButton12);
+        RadioButton europe = findViewById(R.id.radioButton13);
         checkbox = findViewById(R.id.Radiogroup);
+        start = findViewById (R.id.button2);
+        start.setOnClickListener();
     }
 }
