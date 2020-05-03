869aa5e666167ee2453dda53f9455967d339138d
jieshuh2 <jieshuh2@illinois.edu>
2020-05-03 01:18:27 -0500

    New

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,41 +1,65 @@
 package com.example.project;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends AppCompatActivity {
-    public int McDonald;
-    public int EVO;
-    public int PVP;
-    public int FatSandwich;
-    public int LaiLai;
-    public int Teamoji;
-    public int Sakanaya;
-    public RadioGroup checkbox;
-    public Button start;
-    public View map;
+import java.lang.annotation.Target;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+    private CheckBox starving, vegetarian, diet, chinese, japanese, korean, spicy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        map = findViewById(R.id.mapView2);
-        map.setVisibility(View.GONE);
-        RadioButton starving = findViewById(R.id.radioButton6);
-        RadioButton diet = findViewById(R.id.radioButton9);
-        RadioButton spicy = findViewById(R.id.radioButton7);
-        RadioButton quick = findViewById(R.id.radioButton8);
-        RadioButton vegetarian = findViewById(R.id.radioButton10);
-        RadioButton asian = findViewById(R.id.radioButton11);
-        RadioButton american = findViewById(R.id.radioButton12);
-        RadioButton europe = findViewById(R.id.radioButton13);
-        checkbox = findViewById(R.id.Radiogroup);
-        start = findViewById (R.id.button2);
+        findViewById (R.id.button2).setOnClickListener(v -> {
+                Dialog checkbox = onCreateDialog(null);
+                checkbox.show();
+        });
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_API);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapAPI = googleMap;
+    }
+    public Dialog onCreateDialog(final Bundle savedInstanceState){
+        setContentView(R.layout.checkbox);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.checkbox, null);
+        builder.setView(view)
+                .setPositiveButton("Start", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        // User cancelled the dialog
+                        dialog.cancel();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
 }
